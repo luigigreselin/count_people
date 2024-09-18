f5cbad565376452e5f3de8c71f5fad453c07f6cd
luigi.greselin@futurefacts.nl <luigi.greselin@futurefacts.nl>
2024-09-18 16:53:24 +0200

    new precommit config

--- object_detection_api/main.py
+++ object_detection_api/main.py
@@ -1,22 +1,24 @@
+from __future__ import annotations
+
 from fastapi import FastAPI
 from ultralytics import YOLO  # type: ignore
 
 app = FastAPI()
 
 
-@app.get("/")
+@app.get('/')
 async def root():
-    return {"message": "Hello World"}
+    return {'message': 'Hello World'}
 
 
-model = YOLO("yolov8n.pt")
+model = YOLO('yolov8n.pt')
 
 
-@app.post("/detect/")
+@app.post('/detect/')
 async def detect_objects(detected_class: list[int] = [0]):
 
     results = model.predict(
-        source="0", show=True, classes=detected_class, conf=0.8
+        source='0', show=True, classes=detected_class, conf=0.8,
     )  # Display preds. Accepts all YOLO predict arguments
 
-    return {"detections": results}
+    return {'detections': results}
