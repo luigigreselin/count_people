16c6348ccda474912e9fe57355595c43deb26f8d
luigi.greselin@futurefacts.nl <luigi.greselin@futurefacts.nl>
2024-10-01 09:50:34 +0200

    futurefacts

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,17 +1,17 @@
-from fastapi import FastAPI
-from fastapi.testclient import TestClient
-import pytest
+from __future__ import annotations
 
 from count_people.object_detection_api.main import app
+from fastapi.testclient import TestClient
 
 
 client = TestClient(app)
 
 
 def test_get():
-    response = client.get("/")
+    response = client.get('/')
     assert response.status_code == 200
-    print("HERE" ,response)
-    assert "text/html" in response.headers["Content-Type"]
-    assert "<p>Drag & Drop your files here</p>" in response.text  # Check for content in the response
-    # assert "minimum_user_response_time" in response.text  
+    print('HERE', response)
+    assert 'text/html' in response.headers['Content-Type']
+    # Check for content in the response
+    assert '<p>Drag & Drop your files here</p>' in response.text
+    # assert "minimum_user_response_time" in response.text
